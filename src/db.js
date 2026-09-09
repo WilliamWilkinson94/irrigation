ea956b6fbeb8a27937e21b24f4f19597fe7cd149
WilliamWilkinson94 <102689782+WilliamWilkinson94@users.noreply.github.com>
2026-09-09 10:04:48 -0400

    Initialize Prisma Client in db.js

--- src/db.js
+++ src/db.js
@@ -1,1 +1,8 @@
+const { PrismaClient } = require('@prisma/client');
 
+// Instantiate Prisma Client
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'], // Optional: logs SQL queries to console during dev
+});
+
+module.exports = prisma;
