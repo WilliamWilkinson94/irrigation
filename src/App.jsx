883431ab2b29b24a9ea90c7be4ba09360f95d7f7
WilliamWilkinson94 <102689782+WilliamWilkinson94@users.noreply.github.com>
2026-09-10 17:36:01 -0400

    Add IrrigationDashboard component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
+import IrrigationDashboard from './components/IrrigationDashboard';
 
+export default function App() {
+  return (
+    <div>
+      <IrrigationDashboard />
+    </div>
+  );
+}
